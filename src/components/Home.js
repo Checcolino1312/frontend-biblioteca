fbcaa250e33d4f294a705ef897023c07b737edb3
Checcolino1312 <75864171+Checcolino1312@users.noreply.github.com>
2025-06-05 10:27:11 +0200

    commit necessaria

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Link } from 'react-router-dom';
-import './Home.css';
-const Home = () => (_jsxs("div", { className: "home", children: [_jsx("h1", { children: "Benvenuto nella Biblioteca Comunale" }), _jsx("p", { children: "Gestisci i tuoi prestiti e consulta i libri disponibili con facilit\u00E0." }), _jsx(Link, { to: "/registra-prestito", className: "cta-button", children: "Inizia ora" })] }));
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Container, Typography } from '@mui/material';
+const Home = () => (_jsxs(Container, { sx: { py: 4, textAlign: 'center' }, children: [_jsx(Typography, { variant: "h4", gutterBottom: true, children: "Benvenuto nella Biblioteca Comunale" }), _jsx(Typography, { variant: "body1", sx: { mb: 2 }, children: "Gestisci i tuoi prestiti e consulta i libri disponibili con facilit\u00E0." }), _jsx(Button, { variant: "contained", component: RouterLink, to: "/registra-prestito", children: "Inizia ora" })] }));
 export default Home;
